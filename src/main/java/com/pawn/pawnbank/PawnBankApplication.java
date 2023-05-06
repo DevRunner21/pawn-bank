0522f8160993de0874ad8ab17ca2ef4526834ca4
pawn <devrunner21@gmail.com>
2023-05-06 12:10:56 +0900

    Project Init

---
+++ src/main/java/com/pawn/pawnbank/PawnBankApplication.java
@@ -0,0 +1,13 @@
+package com.pawn.pawnbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawnBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawnBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pawn/pawnbank/PawnBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawn.pawnbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawnBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
